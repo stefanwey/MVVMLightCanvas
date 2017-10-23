214922a85d17c674ceffd61fd41267cf4c3b0933
Stefan <stefan@ritawey.ch>
2017-10-23 22:12:49 +0200

    Aufgeräumt.

--- MvvmLight/CanvasGui/MainWindow.xaml.cs
+++ MvvmLight/CanvasGui/MainWindow.xaml.cs
@@ -26,14 +26,6 @@ namespace CanvasGui
         public MainWindow()
         {
             InitializeComponent();
-
-            //_beetliList = new MainViewModel();
-
-            //_beetliList.TheBeetlis.Add(new BeetliData { BeetliLeft = 50, BeetliTop = 50, BeetliHeight = 50, BeetliWidth = 50 });
-            //_beetliList.TheBeetlis.Add(new BeetliData { BeetliLeft = 150, BeetliTop = 100, BeetliHeight = 100, BeetliWidth = 25 });
-            //_beetliList.TheBeetlis.Add(new BeetliData { BeetliLeft = 250, BeetliTop = 50, BeetliHeight = 25, BeetliWidth = 100 });
-
-            //this.DataContext = _beetliList;
         }
     }
 }
